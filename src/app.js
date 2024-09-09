c030fe8dc1511246cd9275844aa8fa3a28f0e40c
Phuc Pham <phamphuc01072003@gmail.com>
2024-09-09 13:41:56 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     return res.json(JSend.success());
 });
+
+app.use('/public', express.static('public'));
 contactsRouter.setup(app);
 
 // Handle 404 response
