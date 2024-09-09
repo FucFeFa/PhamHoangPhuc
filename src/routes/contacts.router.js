da85d8289c6cfced67b23ccec908cac11091d80f
Phuc Pham <phamphuc01072003@gmail.com>
2024-09-09 13:37:23 +0700

    commit

--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const contactsController = require('../controllers/contacts.controller');
+const { methodNotAllowed } = require('../controllers/errors.controller');
+
 const router = express.Router();
 module.exports.setup = (app) => {
-app.use('/api/v1/contacts', router);
-router.get('/', contactsController.getContactsByFilter);
-router.post('/', contactsController.createContact);
-router.delete('/', contactsController.deleteAllContacts);
-router.get('/:id', contactsController.getContact);
-router.put('/:id', contactsController.updateContact);
-router.delete('/:id', contactsController.deleteContact);
+    app.use('/api/v1/contacts', router);
+
+    router.get('/', contactsController.getContactsByFilter);
+    router.post('/', contactsController.createContact);
+    router.delete('/', contactsController.deleteAllContacts);
+    router.all('/', methodNotAllowed);
+    
+    router.get('/:id', contactsController.getContact);
+    router.put('/:id', contactsController.updateContact);
+    router.delete('/:id', contactsController.deleteContact);
+    router.all('/:id', methodNotAllowed);
 };
