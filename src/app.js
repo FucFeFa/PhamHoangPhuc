d05d971340520ae7db2da701dd8d43a28b5dc936
Phuc Pham <phamphuc01072003@gmail.com>
2024-09-09 09:36:38 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const contactsRouter = require('./routes/contacts.router');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,4 +10,5 @@ return res.json({
 message: 'ok'
 });
 });
+contactsRouter.setup(app);
 module.exports = app;
